c369e416652e0e44542a75faf8ff0264e388da4e
Evan-Codes88 <Evan2401tm@gmail.com>
2025-04-02 18:07:27 +1000

    Feat: Fixed MongoDB Connection

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     try {
        const connection = await mongoose.connect(process.env.MONGODB_URI);
-       console.log(`MongoDB Connected: ${connection.Connection.host}`);
+       console.log(`MongoDB Connected: ${connection.connection.host}`);
     } catch (error) {
         console.log("Error connecting to MongoDB: ", error.message);
         process.exit(1);
